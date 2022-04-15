5ec80a18822ad397b4b40d02c8d10efb0f5d399e
N3dal <nedalxzo@gmail.com>
2022-04-15 16:50:06 +0200

    create clear function

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------------
+# simple password generator cli.
+# this is one of my 50 python project challenge for 2021.
+#
+#
+#
+# Author:N84.
+#
+# Create Date:Fri Apr 15 16:45:25 2022.
+# ///
+# ///
+# ///
+# -----------------------------------------------------------------
+
+import string
+from sys import argv
+import random
+from os import name as OS_NAME
+from os import system
+from os import getcwd
+
+
+def clear():
+    """wipe terminal screen."""
+
+    if OS_NAME == "posix":
+        # *nix machines.
+        system("clear")
+
+    else:
+        # windows machines.
+        system("cls")
+
+
+clear()
